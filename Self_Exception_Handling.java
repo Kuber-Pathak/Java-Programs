2fd02e391e6838a2b81849da86038f4af45d2d0f
Kuber-Pathak <kuber.211417@ncit.edu.np>
2023-12-18 18:20:36 +0545

    added

--- Self_Exception_Handling.java
+++ Self_Exception_Handling.java
@@ -1,25 +1,28 @@
 import java.util.*;
-class MyException extends Exception{
-    public MyException(String str){
+
+class MyException extends Exception {
+    public MyException(String str) {
         super(str);
     }
 }
-class Self_Exception_Handling{
-    public static void main(String[] args){
+
+class Self_Exception_Handling {
+    public static void main(String[] args) {
         int num;
         Scanner s = new Scanner(System.in);
         System.out.println("Enter a number greater than 20:");
-        num=s.nextInt();
-        try{
-            if(num<20){
+        num = s.nextInt();
+        s.close();
+        try {
+            if (num < 20) {
                 throw new MyException("Abey aandha he kya ,number must be greater than 20.");
+
+            } else {
+                System.out.println("Num = " + num);
             }
-            else{
-                System.out.println("Num = "+num);
-            }
-        }
-        catch(MyException e){
+        } catch (MyException e) {
             System.out.println(e.getMessage());
         }
+
     }
 }

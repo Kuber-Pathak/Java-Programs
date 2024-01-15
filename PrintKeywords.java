8d9d3cf6f487def5c631ca58ef88b623d4e0ca19
Kuber-Pathak <kuberpathak124@gmail.com>
2024-01-15 22:16:09 +0545

    addded

---
+++ PrintKeywords.java
@@ -0,0 +1,21 @@
+
+//"println" prints a line and moves the cursor to the next line
+// "print" prints a line and keeps the cursor on the same line
+//"\n" is used to print a new line
+
+public class PrintKeywords {
+    public static void main(String[] args) {
+        // println
+        System.out.println("Hello, I am learning Java!");
+        System.out.println("Hello,I am learning Java!");
+
+        // print
+        System.out.print("Hello, I am learning Java!");
+        System.out.print("Hello,I am learning Java!");
+
+        // \n
+        System.out.println("\n");
+        System.out.println("Hello, I am learning Java!\n");
+        System.out.println("Hello,I am learning Java!\n");
+    }
+}

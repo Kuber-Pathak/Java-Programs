423009864ea751759dc45aaffcf8ba9ac32ccfae
Kuber-Pathak <kuberpathak124@gmail.com>
2024-01-25 09:24:13 +0545

    added socket

---
+++ Socket/Client.java
@@ -0,0 +1,29 @@
+package Socket;
+
+import java.net.*;
+import java.util.Scanner;
+import java.io.*;
+
+public class Client {
+    public static void main(String[] args) throws Exception {
+        Socket scc = new Socket("localhost", 12345);
+        InputStream is = scc.getInputStream();
+        DataInputStream dis = new DataInputStream(is);
+        OutputStream os = scc.getOutputStream();
+        DataOutputStream dos = new DataOutputStream(os);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int num = sc.nextInt();
+        dos.writeInt(num);
+        dos.flush();
+        int result = dis.readInt();
+        System.out.println("The result is :" + result);
+        sc.close();
+        dos.close();
+        os.close();
+        dis.close();
+        is.close();
+        scc.close();
+
+    }
+}
---
+++ Socket/Server.java
@@ -0,0 +1,24 @@
+package Socket;
+
+import java.net.*;
+import java.io.*;
+
+public class Server {
+    public static void main(String[] args) throws Exception {
+        ServerSocket ss = new ServerSocket(12345);
+        Socket clientSocket = ss.accept();
+        InputStream is = clientSocket.getInputStream();
+        DataInputStream dis = new DataInputStream(is);
+        OutputStream os = clientSocket.getOutputStream();
+        DataOutputStream dos = new DataOutputStream(os);
+        int num = dis.readInt();
+        dos.writeInt(num*num);
+        dos.flush();
+        dos.close();
+        os.close();
+        dis.close();
+        is.close();
+        clientSocket.close();
+        ss.close();
+    }
+}
